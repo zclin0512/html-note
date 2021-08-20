0b843bdc4d6f2424a8b5556f42a3f29ea1534777
zclin0512 <zclin512@163.com>
2021-08-20 22:47:29 +0800

    添加axios

---
+++ axios封装/request.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+// 创建axios实例化
+const request = axios.create({
+    // baseURL:'http://122.112.203.138:5000',
+    baseURL:'http://127.0.0.1:5000',
+    timeout:5000
+})
+// 请求拦截
+request.interceptors.request.use(config=>{
+    return config
+},err=>{
+    console.log('err')
+})
+// 响应拦截
+request.interceptors.response.use(res=>{
+    return res.data
+},err=>{
+    console.log('err')
+})
+
+
+export default request
+
+
+
+// 使用方法
+// get请求
+request.get("/").then(res=>{})
+// post请求
+request.post("/",data).then(res=>{}）
